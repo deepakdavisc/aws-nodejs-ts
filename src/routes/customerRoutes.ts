59fffe53a863f4bb184c21a671098fccf42bd8cd
Deepak Davis <you@example.com>
2023-03-20 07:59:05 +0530

    api method updated

--- src/routes/customerRoutes.ts
+++ src/routes/customerRoutes.ts
@@ -1,8 +1,11 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { CustomerSignUp } from "../controllers/CustomerController";
 
 const router = express.Router();
 
+router.post("/", (req: Request, res: Response) => {
+  return res.status(200).send("Hello world");
+});
 router.post("/signup", CustomerSignUp);
 
 export { router as CustomerRouter };
